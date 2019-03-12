a993d971ff77eb38416a43f2872fa5b4918b5397
Benny Megidish <bennymegk@gmail.com>
2019-03-12 14:40:13 +0200

    improved packet error handling

--- api/utils/packet.py
+++ api/utils/packet.py
@@ -10,11 +10,32 @@ class Packet:
         self.query = query
 
     def send(self, timeout=5):
-        response = "'exception': 'Request timeout have been reached'" # todo fix
+        response = None
 
         try:
             response = requests.get('http://%s:%s/%s' % (self.ip, self.port, self.query), timeout=timeout)
-        except requests.exceptions.Timeout as e:
-            print_error('Warning: request timeout have been reached')
+        except requests.exceptions.HTTPError as http_error:
+            print_error('Warning: we have encountered in a http error.\n%s\n' % http_error)
+            response = self._generateEmptyResponse('http')
+        except requests.exceptions.Timeout as timeout_error:
+            print_error('Warning: request timeout have been reached.\n%s\n' % timeout_error)
+            response = self._generateEmptyResponse('timeout')
+        except requests.exceptions.ConnectionError as connection_error:
+            print_error('Warning: we have encountered in a connection error.\n%s\n' % connection_error)
+            response = self._generateEmptyResponse('connection')
+        except requests.exceptions.RequestException as error:
+            print_error('Warning: request failed.\n%s\n' % error)
+            response = self._generateEmptyResponse()
+
+        return response
+
+    @staticmethod
+    def _generateEmptyResponse(errorType=b'unknown'):
+        response = requests.Response()
+
+        response._content = b'{ "exception": "Request resolved with an %s error" }' % errorType.encode("ascii")
+        response.code = "expired"
+        response.error_type = "expired"
+        response.status_code = 400
 
         return response
