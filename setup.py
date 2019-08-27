4d63bafd93b4b72fce9a20de5da46697ad8191cf
Benny Megidish <bennymegk@gmail.com>
2019-08-27 20:59:20 +0300

    updated setup file

--- setup.py
+++ setup.py
@@ -7,19 +7,35 @@ with open('README.md', encoding="utf8") as f:
 with open('LICENSE') as f:
     license = f.read()
 
+# install_requires = list()
+# with open('requirements.txt', 'r', encoding='utf-8') as f:
+#     for line in f.readlines():
+#         install_requires.append(line.split("=")[0].strip("<~>"))
+
 setup(
     name='butter.mas-api',
-    version='0.11.11',
+    version='0.11.12',
     description='Python Client API for Butter MAS platform',
     long_description_content_type='text/markdown',
     long_description=readme,
     author='Benny Megidish',
     author_email='bennymegk@gmail.com',
-    url='https://github.com/benymeg/Butter.MAS.PythonAPI',
+    url='https://github.com/bennymeg/Butter.MAS.PythonAPI',
     install_requires=['requests'],
     #license=license,
     license='Apache-2.0',
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+    ],
 )
 
 ''' how to deploy '''
